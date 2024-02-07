77e40fd49513aabacc73a4a2f310242c4d7dc53f
Aaron Christiason <ninjaaron@gmail.com>
2024-02-07 07:48:59 +0100

    add more type information to Run.t

--- lib/run.ml
+++ lib/run.ml
@@ -1,74 +1,65 @@
 open Base
 open Sub2
 
-type 'out t =
+type ('out, 'err) t =
   { proc: Exit.t
   ; stdout: 'out
-  ; stderr: 'out
+  ; stderr: 'err
   }
+
+let get_out t = t.stdout
+let get_err t = t.stderr
+
+let _unchecked reader _ cmd =
+  let proc, (stdout, stderr) = in_context cmd ~f:(fun t ->
+      reader t
+    ) in {proc; stdout; stderr}
+
+let _res reader post cmd =
+  let t = _unchecked reader () cmd in
+  Result.map ~f:(fun _ -> post t) (Exit.check t.proc)
+
+let _exn reader post cmd =
+  Or_error.ok_exn (_res reader post cmd |> or_error)
 
 module type S = sig
   type out
   val reader : In_channel.t -> out
-  val empty : out
 end
 
 module Make(M : S)  = struct
-  let get_reader : type o. o Out.t -> (o -> M.out) = function
-    | Out.Pipe -> M.reader
-    | _ -> fun _ -> M.empty
 
-  let _unchecked cmd =
-    let get_out = get_reader cmd.Cmd.stdout in
-    let get_err = get_reader cmd.Cmd.stderr in
-    let proc, (stdout, stderr) = in_context cmd ~f:(fun t ->
-        get_out t.stdout, get_err t.stderr
-      ) in {proc; stdout; stderr}
+  let _out_helper f cmd =
+    cmd |> pipe |> f Sub2.(fun t -> M.reader t.stdout, t.stderr) get_out
+  let _err_helper f cmd =
+    cmd |> pipe_err |> f Sub2.(fun t -> t.stdout, M.reader t.stderr) get_err
+  let _both_helper f cmd =
+    cmd |> pipe |> pipe_err
+    |> f Sub2.(fun t -> M.reader t.stdout, M.reader t.stderr) Fn.id
 
-  let _res out_mapper cmd =
-    let t = _unchecked cmd in
-    Result.map ~f:(fun _ -> out_mapper t) (Exit.check t.proc)
-    
-  let _exn out_mapper cmd =
-    Or_error.ok_exn (_res out_mapper cmd |> or_error)
-
-  let _get_out t = t.stdout
-  let _get_err t = t.stderr
-
-  let _out_helper f args = cmd args |> pipe |> f
-  let _err_helper f args = cmd args |> pipe_err |> f
-  let _both_helper f args = cmd args |> pipe |> pipe_err |> f
-
-  let unchecked = _out_helper _unchecked
-  let res = _out_helper (_res _get_out)
-  let exn = _out_helper (_exn _get_out)
-  let unchecked_err = _err_helper _unchecked
-  let res_err = _err_helper (_res _get_err)
-  let exn_err = _err_helper (_exn _get_err)
-  let unchecked_both = _both_helper _unchecked
-  let res_both = _both_helper (_res Fn.id)
-  let exn_both = _both_helper (_exn Fn.id)
+  let unchecked cmd = _out_helper _unchecked cmd
+  let res cmd = _out_helper _res cmd
+  let exn cmd = _out_helper _exn cmd
+  let err_unchecked cmd = _err_helper _unchecked cmd
+  let err_res cmd = _err_helper _res cmd
+  let err_exn cmd = _err_helper _exn cmd
+  let both_unchecked cmd = _both_helper _unchecked cmd
+  let both_res cmd = _both_helper _res cmd
+  let both_exn cmd = _both_helper _exn cmd
 end
 
 module Read = Make(struct
     type out = string
     let reader = In_channel.input_all
-    let empty = ""
   end)
 
 module Lines = Make(struct
     type out = string list
     let reader = In_channel.input_lines
-    let empty = []
   end)
 
-module UnitOut = Make(struct
-    type out = unit
-    let reader = fun _ -> ()
-    let empty = ()
-  end)
+let _neither t = Sub2.(t.stdout, t.stderr)
 
-let unchecked args = UnitOut._unchecked (cmd args)
-let res args = UnitOut._res (fun _ -> ()) (cmd args)
-let exn args = UnitOut._exn (fun _ -> ()) (cmd args)
-let (let*) = (let*)
+let unchecked cmd = _unchecked _neither () cmd
+let res cmd = _res _neither (fun _ -> ()) cmd
+let exn cmd = _exn _neither (fun _ -> ()) cmd
--- lib/run.mli
+++ lib/run.mli
@@ -1,30 +1,31 @@
-type 'out t = { proc : Exit.t; stdout : 'out; stderr : 'out; }
+open Sub2
+type ('out, 'err) t = { proc : Exit.t; stdout : 'out; stderr : 'err; }
 
 module Read : sig
-  val unchecked : string array -> string t
-  val res : string array -> (string, Exit.t) result
-  val exn : string array -> string
-  val unchecked_err : string array -> string t
-  val res_err : string array -> (string, Exit.t) result
-  val exn_err : string array -> string
-  val unchecked_both : string array -> string t
-  val res_both : string array -> (string t, Exit.t) result
-  val exn_both : string array -> string t
+  val unchecked : ('i, stdout, 'e) Cmd.t -> (string, 'e) t
+  val res : ('i, stdout, 'e) Cmd.t -> (string, Exit.t) result
+  val exn : ('i, stdout, 'e) Cmd.t -> string
+  val err_unchecked : ('i, 'o, stderr) Cmd.t -> ('o, string) t
+  val err_res : ('i, 'o, stderr) Cmd.t -> (string, Exit.t) result
+  val err_exn : ('i, 'o, stderr) Cmd.t -> string
+  val both_unchecked : ('i, stdout, stderr) Cmd.t -> (string, string) t
+  val both_res :
+    ('i, stdout, stderr) Cmd.t -> ((string, string) t, Exit.t) result
+  val both_exn : ('i, stdout, stderr) Cmd.t -> (string, string) t
 end
 
 module Lines : sig
-  val unchecked : string array -> string list t
-  val res : string array -> (string list, Exit.t) result
-  val exn : string array -> string list
-  val unchecked_err : string array -> string list t
-  val res_err : string array -> (string list, Exit.t) result
-  val exn_err : string array -> string list
-  val unchecked_both : string array -> string list t
-  val res_both : string array -> (string list t, Exit.t) result
-  val exn_both : string array -> string list t
+  val unchecked : ('i, stdout, 'e) Cmd.t -> (string list, 'e) t
+  val res : ('i, stdout, 'e) Cmd.t -> (string list, Exit.t) result
+  val exn : ('i, stdout, 'e) Cmd.t -> string list
+  val err_unchecked : ('i, 'o, stderr) Cmd.t -> ('o, string list) t
+  val err_res : ('i, 'o, stderr) Cmd.t -> (string list, Exit.t) result
+  val err_exn : ('i, 'o, stderr) Cmd.t -> string list
+  val both_unchecked : ('i, stdout, stderr) Cmd.t -> (string list, string list) t
+  val both_res : ('i, stdout, stderr) Cmd.t -> ((string list, string list) t, Exit.t) result
+  val both_exn : ('i, stdout, stderr) Cmd.t -> (string list, string list) t
 end
 
-val unchecked : string array -> unit t
-val res : string array -> (unit, Exit.t) result
-val exn : string array -> unit
-val ( let* ) : ('a, 'e) result -> ('a -> ('b, 'e) result) -> ('b, 'e) result
+val unchecked : ('i, 'o, 'e) Cmd.t -> ('o, 'e) t
+val res : ('i, 'o, 'e) Cmd.t -> (unit, Exit.t) result
+val exn : ('i, 'o, 'e) Cmd.t -> unit
