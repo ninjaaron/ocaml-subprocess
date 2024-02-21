37290389e940a9b9212e82a34d1037bede316719
Aaron Christiason <ninjaaron@gmail.com>
2024-02-21 19:11:14 +0100

    fix a few mistakes, add some docs and give better type variable names

--- lib/core.mli
+++ lib/core.mli
@@ -1,3 +1,31 @@
+(** Here follows the interface defined in the [Core] module.
+    This module is included in most of the user-facing modules for
+    convenience, so you may stumble across its documentation in
+    several places.
+    
+    The interface defined here is needed to do almost anything with
+    the library.
+    
+    We begin with a set of placeholder types which are used to
+    represent handles to the i/o streams of running processes
+    (namely, stdin, stdout and stderr). Since only streams that have
+    pipes in the parent process are really interactive in any way,
+    these non-interactive place holders are used in other
+    cases. Their main function is to provide type-level information
+    about the streams. *)
+
+type stdin = Stdin (** stdin of the parent process *)
+type stdout = Stdout (** stdout of the parent process *)
+type stderr = Stderr (** stderr of the parent process *)
+type channel = Channel (** stream redirects to/from a channel *)
+type devnull = Devnull (** stream redirects to /dev/null *)
+type file = File of string (** stream redirects to a file which is
+  represented by its name *)
+
+(** The type {!type:t} represents a process which may still be
+    running. Some of the higher-level functions don't expose it
+    directly, but almost everything else is implemented in terms of
+    this type *)
 type ('stdin, 'stdout, 'stderr) t =
   { pid : int
   ; args : string array
@@ -14,13 +42,6 @@ val wait : ?mode:Unix.wait_flag list
 val poll : ('stdin, 'stdout, 'stderr) t -> Unix.process_status option
 
 val check : ('stdin, 'stdout, 'stderr) t -> (Exit.t, Exit.t) result
-
-type stdin = Stdin
-type stdout = Stdout
-type stderr = Stderr
-type channel = Channel
-type devnull = Devnull
-type file = File of string
 
 module In : sig
   type _ t =
@@ -51,40 +72,54 @@ end
 
 val cmd : string array -> (stdin, stdout, stderr) Cmd.t
 
-val set_in : 'stdin2 In.t
-  -> ('stdin1, 'stdout, 'stderr) Cmd.t
-  -> ('stdin2, 'stdout, 'stderr) Cmd.t
-val set_out : 'stdout2 Out.t
-  -> ('stdin, 'stdout1, 'stderr) Cmd.t
-  -> ('stdin, 'stdout2, 'stderr) Cmd.t
-val set_err : 'stderr2 Out.t
-  -> ('stdin, 'stdout, 'stderr1) Cmd.t
-  -> ('stdin, 'stdout, 'stderr2) Cmd.t
-val pipe_in : (stdin, 'stdout, 'stderr) Cmd.t
-  -> (out_channel, 'stdout, 'stderr) Cmd.t
-val pipe_out : ('stdin, stdout, 'stderr) Cmd.t
-  -> ('stdin, in_channel, 'stderr) Cmd.t
-val pipe_err : ('stdin, 'stdout, stderr) Cmd.t
-  -> ('stdin, 'stdout, in_channel) Cmd.t
-val channel_in : in_channel
-  -> (stdin, 'stdout, 'stderr) Cmd.t
-  -> (channel, 'stdout, 'stderr) Cmd.t
-val channel_out : out_channel
-  -> ('stdin, stdout, 'stderr) Cmd.t
-  -> ('stdin, channel, 'stderr) Cmd.t
-val channel_err : out_channel
-  -> ('stdin, 'stdout, stderr) Cmd.t
-  -> ('stdin, 'stdout, channel) Cmd.t
-val file_in : string
-  -> (stdin, 'stdout, 'stderr) Cmd.t
-  -> (file, 'stdout, 'stderr) Cmd.t
-val file_out : string
-  -> ('stdin, stdout, 'stderr) Cmd.t
-  -> ('stdin, file, 'stderr) Cmd.t
-val file_err : string
-  -> ('stdin, 'stdout, stderr) Cmd.t
-  -> ('stdin, 'stdout, file) Cmd.t
-val devnull_out : ('stdin, stdout, 'stderr) Cmd.t
-  -> ('stdin, devnull, 'stderr) Cmd.t
-val devnull_err : ('stdin, 'stdout, stderr) Cmd.t
-  -> ('stdin, 'stdout, devnull) Cmd.t
+val set_in :
+  'new_stdin In.t ->
+  (stdin, 'stdout, 'stderr) Cmd.t ->
+  ('new_stdin, 'stdout, 'stderr) Cmd.t
+val set_out :
+  'new_stdout Out.t ->
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  ('stdin, 'new_stdout, 'stderr) Cmd.t
+val set_err :
+  'new_stderr Out.t ->
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  ('stdin, 'stdout, 'new_stderr) Cmd.t
+val pipe_in :
+  (stdin, 'stdout, 'stderr) Cmd.t ->
+  (out_channel, 'stdout, 'stderr) Cmd.t
+val pipe_out :
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  ('stdin, in_channel, 'stderr) Cmd.t
+val pipe_err :
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  ('stdin, 'stdout, in_channel) Cmd.t
+val channel_in :
+  in_channel ->
+  (stdin, 'stdout, 'stderr) Cmd.t ->
+  (channel, 'stdout, 'stderr) Cmd.t
+val channel_out :
+  out_channel ->
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  ('stdin, channel, 'stderr) Cmd.t
+val channel_err :
+  out_channel ->
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  ('stdin, 'stdout, channel) Cmd.t
+val file_in :
+  string ->
+  (stdin, 'stdout, 'stderr) Cmd.t ->
+  (file, 'stdout, 'stderr) Cmd.t
+val file_out :
+  string ->
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  ('stdin, file, 'stderr) Cmd.t
+val file_err :
+  string ->
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  ('stdin, 'stdout, file) Cmd.t
+val devnull_out :
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  ('stdin, devnull, 'stderr) Cmd.t
+val devnull_err :
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  ('stdin, 'stdout, devnull) Cmd.t
---
+++ lib/subprocess.mli
@@ -0,0 +1,106 @@
+module Core = Core
+include module type of Core
+
+val exec : ('stdin, 'stdout, 'stderr) Cmd.t ->
+  f:(('stdin, 'stdout, 'stderr) t -> 'a) ->
+  'a
+
+val run : ('stdin, 'stdout, 'stderr) Cmd.t -> unit
+val read : ('stdin, stdout, 'stderr) Cmd.t -> string
+val lines : ('stdin, stdout, 'stderr) Cmd.t -> string list
+val read_err : ('stdin, 'stdout, stderr) Cmd.t -> string
+val lines_err : ('stdin, 'stdout, stderr) Cmd.t -> string list
+val fold :
+  ('stdin, stdout, 'stderr) Cmd.t ->
+  f:('acc -> string -> 'acc) ->
+  init:'acc ->
+  'acc
+val fold_err :
+  ('stdin, 'stdout, stderr) Cmd.t ->
+  f:('acc -> string -> 'acc) ->
+  init:'acc ->
+  'acc
+
+module Exit = Exit
+
+module Results : sig
+  val exec : 
+    ('stdin, 'stdout, 'stderr) Cmd.t ->
+    f:(('stdin, 'stdout, 'stderr) t -> ('a, Exit.t) result) ->
+    ('a, Exit.t) result
+  val run : ('stdin, 'stdout, 'stderr) Cmd.t -> (unit, Exit.t) result
+  val read : ('stdin, stdout, 'stderr) Cmd.t -> (string, Exit.t) result
+  val lines : ('stdin, stdout, 'stderr) Cmd.t -> (string list, Exit.t) result
+  val read_err : ('stdin, 'stdout, stderr) Cmd.t -> (string, Exit.t) result
+  val lines_err : ('stdin, 'stdout, stderr) Cmd.t -> (string list, Exit.t) result
+  val fold : 
+    ('stdin, stdout, 'stderr) Cmd.t ->
+    f:('acc -> string -> 'acc) ->
+    init:'acc ->
+    ('acc, Exit.t) result
+  val fold_err : 
+    ('stdin, 'stdout, stderr) Cmd.t ->
+    f:('acc -> string -> 'acc) ->
+    init:'acc ->
+    ('acc, Exit.t) result
+
+  val (let*) : 
+    ('a, 'b) result ->
+    ('a -> ('c, 'b) result) ->
+    ('c, 'b) result
+  val (let|) : 
+    ('stdin, 'stderr, 'stdout) Cmd.t ->
+    (('stdin, 'stderr, 'stdout) t -> ('a, Exit.t) result) ->
+    ('a, Exit.t) result
+
+  include module type of Core
+end
+
+module Or_error : sig
+  val exec : 
+    ('stdin, 'stdout, 'stderr) Cmd.t ->
+    f:(('stdin, 'stdout, 'stderr) t -> ('a, Base.Error.t) result) ->
+    ('a, Base.Error.t) result
+  val run : ('stdin, 'stdout, 'stderr) Cmd.t -> (unit, Base.Error.t) result
+  val read : ('stdin, stdout, 'stderr) Cmd.t -> (string, Base.Error.t) result
+  val lines : ('stdin, stdout, 'stderr) Cmd.t -> (string list, Base.Error.t) result
+  val read_err : ('stdin, 'stdout, stderr) Cmd.t -> (string, Base.Error.t) result
+  val lines_err : ('stdin, 'stdout, stderr) Cmd.t -> (string list, Base.Error.t) result
+  val fold : 
+    ('stdin, stdout, 'stderr) Cmd.t ->
+    f:('acc -> string -> 'acc) ->
+    init:'acc ->
+    ('acc, Base.Error.t) result
+  val fold_err : 
+    ('stdin, 'stdout, stderr) Cmd.t ->
+    f:('acc -> string -> 'acc) ->
+    init:'acc ->
+    ('acc, Base.Error.t) result
+
+  val (let*) : 
+    ('a, 'b) result ->
+    ('a -> ('c, 'b) result) ->
+    ('c, 'b) result
+  val (let|) : 
+    ('stdin, 'stderr, 'stdout) Cmd.t ->
+    (('stdin, 'stderr, 'stdout) t -> ('a, Base.Error.t) result) ->
+    ('a, Base.Error.t) result
+
+  include module type of Core
+end
+
+module Unchecked : sig
+  val exec : ('stdin, 'stdout, 'stderr) Cmd.t ->
+    f:(('stdin, 'stdout, 'stderr) t -> 'a) ->
+    Exit.t * 'a
+  val run : ('stdin, 'stdout, 'stderr) Cmd.t -> ('stdin, 'stdout, 'stderr) Run.t
+  val read : ('stdin, stdout, 'stderr) Cmd.t -> ('stdin, string, 'stderr) Run.t
+  val lines : ('stdin, stdout, 'stderr) Cmd.t -> ('stdin, string list, 'stderr) Run.t
+  val read_err : ('stdin, 'stdout, stderr) Cmd.t -> ('stdin, 'stdout, string) Run.t
+  val lines_err : ('stdin, 'stdout, stderr) Cmd.t -> ('stdin, 'stdout, string list) Run.t
+
+  include module type of Core
+end
+
+module Exec = Exec
+
