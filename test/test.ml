bd517a7b79dc4c8d0efb83d6c61c1a6d2627f91f
Aaron Christiason <ninjaaron@gmail.com>
2025-04-05 20:58:56 +0200

    added a couple more tests

--- test/test.ml
+++ test/test.ml
@@ -5,6 +5,17 @@ let print_to_stdout = with_shell "echo 'foo\nbar\nbaz'"
 let print_to_stderr = with_shell ">&2 echo 'foo\nbar\nbaz'"
 let non_zero = Subprocess.cmd ["false"]
 let error_with_output = with_shell "echo 'message'; false"
+let long_output = with_shell {|
+    for line in $(seq 1 99999); do echo "foo"; done
+    |}
+
+let out_and_err = with_shell {|
+    for n in $(seq 1 100); do
+      echo "out $n, part 1"
+      >&2 echo "error $n"
+      echo "out $n, part 2"
+    done
+    |}
 
 module TestExceptionsInterface = struct
 
@@ -80,6 +91,27 @@ module TestExceptionsInterface = struct
       print_endline !output;
     [%expect {| message |}]
 
+  let%expect_test "multi-pipe with long output" =
+    let lines =
+      let& proc1 = long_output |> pipe_out in
+      let& proc2 =
+        cmd ["tr"; "a-z"; "A-Z"] |> channel_in (stdout proc1) |> pipe_out in
+      let& proc3 = cmd ["cat"] |> channel_in (stdout proc2) |> pipe_out in
+      let& proc4 =
+        cmd ["tr"; "A-Z"; "a-z"] |> channel_in (stdout proc3) |> pipe_out in
+      In_channel.input_lines (stdout proc4) in
+    Printf.printf "%S x %n\n" (List.hd lines) (List.length lines);
+    [%expect {| "foo" x 99999 |}]
+
+  let%expect_test "test joined" =
+    let n_lines = lines_joined out_and_err |> List.length in
+    print_int n_lines;
+    [%expect {| 300 |}]
+
+  let%expect_test "test both" =
+    let (out, err) = lines_both out_and_err in
+    Printf.printf "out: %d, err: %d" (List.length out) (List.length err);
+    [%expect {| out: 200, err: 100 |}]
 end
 
 module TestResultsInterface = struct
